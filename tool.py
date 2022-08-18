0a4d4daac790274f812e73921a3b8f5ee69cf103
adgsenpai <adgrules@hotmail.com>
2022-08-18 13:48:35 +0200

    using /mnt/windows11

--- tool.py
+++ tool.py
@@ -18,9 +18,11 @@ def create_bootable_usb_windows(USBDrive):
     # Create a new USB drive
     print('Creating USB drive...')
     subprocess.call(['sudo','mkfs.ntfs','-f', USBDrive,'-F'])
+    # create a new directory to mount the USB drive
+    subprocess.call(['sudo','mkdir','/mnt/windows11usb'])
     # mount USB drive
-    print('Mounting USB drive...')
-    subprocess.call(['sudo','mount','-t',USBDrive,'/windowsdisk'])        
+    print('Mounting USB drive...')    
+    subprocess.call(['sudo','mount',USBDrive,'/mnt/windows11usb'])        
     print('USB drive created successfully.')    
     print('Copying ISO to USB drive...')    
     output = subprocess.run(['sudo','dd','bd=4M','if=windows11.iso','of=/windowsdisk','status=progress','oflag=sync'], capture_output=True)
